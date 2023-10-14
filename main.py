6be8b3b375759844c82cd08219469dd5ffc0c2d7
Hoaper <beeboplay@gmail.com>
2023-10-14 20:20:24 +0600

    Cutting by tokens added. ChatGPT Integration added

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import streamlit as st
-from streamlit_tags import st_tags 
+from streamlit_tags import st_tags
+from langchain.llms.openai import OpenAI
+from dotenv import load_dotenv
+import tiktoken
 import datetime
+
+load_dotenv()
+
+llm = OpenAI()
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+prompt_template = """Я - Фрилансер. Оцени пожалуйста мой профиль от 1 до 10. Будь со мной строг и объективен.
+Распиши все мои плюсы и недостатки как можно подробнее.
+Вот информация обо мне:
+Я ищу работу на позицию: {position}, в сфере: {category}
+Обо мне: {about}
+Вот где я работал: {job_experience}
+Вот где я учился: {education}
+Знаю языки: {languages}
+Мои Hard Skills: {hard_skills}
+Мои Soft Skills: {soft_skills}
+Я ориентируюсь на оплату в: {price} тенге, как думаешь, это подходящая цена?
+"""
+
 
 st.title("Форма для ввода данных фрилансера")
 
@@ -60,7 +81,6 @@ if "educations" not in st.session_state:
 
 for i in range(1, st.session_state.num_education + 1):
     key = f"education_{i}"
-    print(key)
     defaults = st.session_state.educations.get(key, ["", "", "Полный рабочий день", "", "", ""])
 
     with st.expander(f"Образование #{i}", expanded=True):
@@ -77,10 +97,11 @@ for i in range(1, st.session_state.num_education + 1):
             ("Среднее образование", "Бакалавр", "Магистр", "Доктор наук"),
             key=f"degree_{i}"
         )
+        profession = st.text_input("Специальность", key=f"profession_{i}")
         grade = st.number_input("Оценка", key=f"grade_{i}", step=0.5)
         achievements = st_tags(label="Достижения", text="Нажмите Enter чтобы ввести", key=f"achievements_{i}")
 
-        st.session_state.educations[key] = [organization, edu_period, degree, grade, achievements]
+        st.session_state.educations[key] = [organization, edu_period, degree, grade, achievements, profession]
 
 add_education = st.button("Добавить данные об образование")
 if add_education:
@@ -98,9 +119,76 @@ soft_skills = st_tags(label="## Софт скилы", text="Нажмите Enter
 st.header("Финансовая информация")
 hourly_rate = st.number_input("Базовая ставка, которую вы бы хотели получать в тенге", min_value=0, value=0)
 
-submitted = st.button("ИИ Анализ Профиля", type="primary")
 
+def cut_text_by_tokens(text: str, limit: int = 4096):
+    # Cut text for specific amount of tokens
+    tokens = encoding.encode(data)
+    current_part = []
+    current_count = 0
+
+    # SELECT AMOUNT OF TOKENS
+    for token in tokens:
+        current_part.append(token)
+        current_count += 1
+        if current_count >= limit:
+            return "".join([encoding.decode_single_token_bytes(token).decode("utf-8", errors="replace") for token in current_part])
+
+    return text
+
+
+def make_response(data: str):
+    cutted_text = cut_text_by_tokens(data)
+    return llm.predict(cutted_text)
+
+
+submitted = st.button("ИИ Анализ Профиля", type="primary")
 if submitted:
     # Здесь можно добавить код для анализа данных или их сохранения
-    st.success("Данные отправлены успешно!")
+    # hourly_rate domain job_category about
+    job_data = st.session_state.job_experiences.values()
+    edu_data = st.session_state.educations.values()
+    langs = ",".join(languages)
+    h_skills = ",".join(hard_skills)
+    s_skills = ",".join(soft_skills)
+
+    job_template = "Компания: {company} Должность: {domain}. С {started_date} по {end_date}. Приобрел или показал навыки: {skills}"
+    job_experiences = []
+    for job in job_data:
+        # [job_title, company_name, work_type, period, skills, description]
+        job_experiences.append(job_template.format(
+            company=job[1],
+            domain=job[0],
+            started_date=str(job[3][0]),
+            end_date=str(job[3][1]),
+            skills=",".join(skills)
+        ))
+    edu_template = "Заведение: {company} Степень: {degree}. С {started_date} по {end_date}. Специальность: {profession}. На оценку {grade}"
+    edu_experiences = []
+    for edu in edu_data:
+        # [organization, edu_period, degree, grade, achievements, profession] # skeleton of edu
+        edu_experiences.append(edu_template.format(
+            company=edu[0],
+            degree=edu[2],
+            started_date=str(edu[1][0]),
+            end_date=str(edu[1][1]),
+            profession=edu[5],
+            grade=edu[3]
+        ))
+
+    data = prompt_template.format(
+        position=domain, 
+        category=job_category, 
+        about=about, 
+        languages=langs, 
+        hard_skills=h_skills, 
+        soft_skills=s_skills, 
+        price=hourly_rate,
+        education=".".join(edu_experiences),
+        job_experience=".".join(job_experiences)
+    )
+
+    # st.write(data)
+
+    response = make_response(data)
+    st.success(f"Оценка вашего профиля от AI:\n{response}")
 
