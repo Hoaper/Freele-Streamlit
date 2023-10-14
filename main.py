057f875135b30efcece2c8230b23039f41fa0a96
Mukhamejan Karatayev <42678345+MukhamejanKaratayev@users.noreply.github.com>
2023-10-14 15:16:51 +0600

    add job exp multiple

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 import streamlit as st
 from streamlit_tags import st_tags 
-
-def experience_form(experience_number):
-
-    with st.expander(f"Опыт работы #{experience_number}", expanded=True):
-        st.header(f"Опыт работы #{experience_number}")
-        job_title = st.text_input("Job title", key=f"job_title_{experience_number}")
-        company_name = st.text_input("Company name", key=f"company_name_{experience_number}")
-        work_type = st.selectbox("Work-type", ["Полный рабочий день", "Частичная занятость", "Фриланс"], key=f"work_type_{experience_number}")
-        period = st.text_input("Period of work", key=f"period_{experience_number}")
-        skills = st.text_input("Skills", key=f"skills_{experience_number}")
-        description = st.text_area("Description", key=f"description_{experience_number}")
-
-    return {
-        "job_title": job_title,
-        "company_name": company_name,
-        "work_type": work_type,
-        "period": period,
-        "skills": skills,
-        "description": description
-    }
+import datetime
 
 st.title("Форма для ввода данных фрилансера")
 
 st.header("Основная информация")
-domain = st.text_input("В какой сфере ты хочешь работать")
-
+domain = st.text_input("Позиция", placeholder="Например, веб-разработка")
+job_category = st.selectbox(
+   "Категория",
+   ("Веб разроботка", "Мобильная разработка", "Программирование", "Веб дизайн", "Data Science", "Дизайн", "Маркетинг", "Администрирование", "Продажи", "Контент", "Переводы", "Видео", "Аудио", "Бизнес-услуги", "Другое"),
+   index=None,
+   placeholder="Выберите категорию",
+)
+about = st.text_area("Расскажите о себе", placeholder="Например, я начинающий веб-разработчик, который хочет найти работу в этой сфере")
 
 st.header("Опыт работы")
-if 'experiences' not in st.session_state:
-    st.session_state.experiences = []
+
+if "num_experience" not in st.session_state:
+    st.session_state.num_experience = 1
+if "job_experiences" not in st.session_state:
+    st.session_state.job_experiences = {}
+
+for i in range(1, st.session_state.num_experience + 1):
+    key = f"job_experience_{i}"
+    defaults = st.session_state.job_experiences.get(key, ["", "", "Полный рабочий день", "", "", ""])
+
+    with st.expander(f"Опыт работы #{i}", expanded=True):
+        job_title = st.text_input("Job title", value=defaults[0], key=f"job_title_{i}")
+        company_name = st.text_input("Company name", value=defaults[1], key=f"company_name_{i}")
+        work_type_options = ["Полный рабочий день", "Частичная занятость", "Фриланс"]
+        work_type = st.selectbox("Work-type", work_type_options, index=work_type_options.index(defaults[2]), key=f"work_type_{i}")
+        period = st.date_input(
+            "Period of work",
+            (datetime.date(2022, 1, 1), datetime.date(2023, 1, 7)),
+            datetime.date(2022, 1, 1),
+            datetime.date(2023, 1, 7),
+            format="MM.DD.YYYY",
+            key=f"period_{i}"
+        )
+        # skills = st.text_input("Skills", value=defaults[4], key=f"skills_{i}")
+        skills = st_tags(label="Skills", text="Нажмите Enter чтобы ввести", key=f"skills_{i}")
+        description = st.text_area("Description", value=defaults[5], key=f"description_{i}")
+
+        st.session_state.job_experiences[key] = [job_title, company_name, work_type, period, skills, description]
+
 add_experience = st.button("Добавить опыт работы")
 if add_experience:
-    st.session_state.experiences.append(experience_form(len(st.session_state.experiences) + 1))
+    st.session_state.num_experience += 1
 
+st.markdown(st.session_state.job_experiences)
 
 
 st.header("Образование")
@@ -55,7 +70,7 @@ soft_skills = st_tags(label="Soft Skills", text="Нажмите Enter чтобы
 st.header("Финансовая информация")
 hourly_rate = st.number_input("Базовая ставка, которую вы бы хотели получать в тенге", min_value=0, value=0)
 
-submitted = st.button("Анализ")
+submitted = st.button("ИИ Анализ Профиля", type="primary")
 
 if submitted:
     # Здесь можно добавить код для анализа данных или их сохранения
